959dd5d9b41f6a44a6c03b36a866c919579d7ccd
Souvik Banerjee <souvik1997@gmail.com>
2016-04-30 13:44:23 -0500

    reformat

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -10,21 +10,19 @@ import CodeGen._
  */
 object Main {
     def main(args: Array[String]) = {
-	// Some easier tests
-	val Parsed.Success(expr, _) = FunParser.expr_pred2.parse("1 * 2")
-	val Parsed.Success(expr2, _) = FunParser.expr_pred2.parse("1 * 2 + 3")
-	val Parsed.Success(expr3, _) = FunParser.expr_pred2.parse("1 + 2")
+        // Some easier tests
+        val Parsed.Success(expr, _) = FunParser.expr_pred2.parse("1 * 2")
+        val Parsed.Success(expr2, _) = FunParser.expr_pred2.parse("1 * 2 + 3")
+        val Parsed.Success(expr3, _) = FunParser.expr_pred2.parse("1 + 2")
         val result = FunParser.parse("fun main(a, b) \n { \n}")
         result match {
             case Right(x) => { CodeGen.generate(x) }
-            case Left(x) => { println("Error "+ x) }
+            case Left(x) => { println("Error " + x) }
         }
 
-
-	println(expr + " -> " + Optimizer.optimizeExpr(expr))
-	println(expr2 + " -> " + Optimizer.optimizeExpr(expr2))
-	println(expr3 + " -> " + Optimizer.optimizeExpr(expr3))
-
+        println(expr + " -> " + Optimizer.optimizeExpr(expr))
+        println(expr2 + " -> " + Optimizer.optimizeExpr(expr2))
+        println(expr3 + " -> " + Optimizer.optimizeExpr(expr3))
 
     }
 }
