7b166d1265f73e1c7988e4b19247649c0549754a
Souvik Banerjee <souvik1997@gmail.com>
2016-04-30 16:22:35 -0500

    push

--- src/main/scala/CodeGen.scala
+++ src/main/scala/CodeGen.scala
@@ -8,6 +8,16 @@ object CodeGen {
     def generate(program: Syntax.Program) = program.functions.map(generateFunction)
 
     def generateFunction(function: Syntax.Function): Unit = {
-        System.out.printf("define i64 @%s (%s)%n", function.name, function.arguments.map(_.name).mkString(","))
+        System.out.printf("define i64 @%s (%s)%n { \n %s \n}", function.name, function.arguments.map(_.name).mkString(","), generateExpression(function.body))
+    }
+
+    def generateExpression(body: Syntax.Statement): Unit = {
+        if (body.isInstanceOf[Syntax.Sequence]) {
+            body.asInstanceOf[Syntax.Sequence].statements.map(generateExpression _)
+        }
+        /*else if (body.isInstanceOf[Syntax.Print]): Unit = {
+            /* TODO.... */
+        }*/
+
     }
 }
