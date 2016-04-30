a7f9b070e5142d119c6e0d31dc759e902008b8e0
Saket Sadani <sadani@cs.utexas.edu>
2016-04-30 16:06:02 -0500

    Add more simple optimizations for statements

--- src/main/scala/Optimizer.scala
+++ src/main/scala/Optimizer.scala
@@ -10,7 +10,26 @@ object Optimizer {
   def optimizeFunction(function: Syntax.Function) : Syntax.Function =
   	function.copy(body = optimizeStatement(function.body))
 
-  def optimizeStatement(statement: Syntax.Statement) : Syntax.Statement = statement
+  def optimizeStatement(statement: Syntax.Statement) : Syntax.Statement = statement match {
+    case Sequence(statements) => Sequence(statements.map(optimizeStatement))
+    case Print(expr) => Print(optimizeExpr(expr))
+    case Assign(variable, expr) => Assign(variable, optimizeExpr(expr))
+    case Return(expr) => Return(optimizeExpr(expr))
+
+    case If(predicate, trueBody, falseBody) => optimizeExpr(predicate) match {
+      case Constant(0) => falseBody
+      case Constant(_) => trueBody
+      case optimizedPred => If(optimizedPred, trueBody, falseBody)
+    }
+
+    case While(predicate, body) => optimizeExpr(predicate) match {
+      case Constant(0) => NoOp
+      case Constant(_) => body // TODO: Throw an error on infinite loops?
+      case optimizedPred => While(optimizedPred, body)
+    }
+
+    case NoOp => NoOp
+  }
 
   def optimizeExpr(expr: Syntax.Expression) : Syntax.Expression = expr match {
   	case Addition(_left, _right) => (optimizeExpr(_left), optimizeExpr(_right)) match {
@@ -29,6 +48,11 @@ object Optimizer {
 		case _ => expr
 	}
 
+  /*
+   * Optimizes each of the possible equalities. The "tautology" term exists to optimze the case
+   * where the same variable is used and the result depends on whether we are processing an
+   * equality or inequality.
+   */
 	case Equal(_left, _right) => optimizeEquality(optimizeExpr(_left), optimizeExpr(_right), _ == _, true)
 	case LessThan(_left, _right) => optimizeEquality(optimizeExpr(_left), optimizeExpr(_right), _ < _, false)
 	case GreaterThan(_left, _right) => optimizeEquality(optimizeExpr(_left), optimizeExpr(_right), _ > _, false)
@@ -48,3 +72,32 @@ object Optimizer {
   	case (Variable(var1), Variable(var2)) if var1 == var2 => Constant(if(tautology) 1 else 0)
   }
 } 
+
+// POTENTIAL FUTURE OPTIMIZATIONS
+
+/*While loops*/
+/*
+1)Loop unrolling
+2)print nothing if the condition will evaluate to zero
+3)loop invariant code removal/movement-if any computation produces
+the same value over and over, move outside loop (but only if the loop will
+execute at all, otherwise, incorrect and not profitable)
+4)^handling for nested loops??
+*/
+
+/*If statements*/
+/*
+1) removing nesting by combining?
+2) reducing number of if statements by combining into 1 if they share same conditions
+*/
+
+
+/*Functions*/
+/*
+Might be a little too hard, but some simple recursion elemination?
+*/
+
+/*Multiplication Addendum*/
+/*
+1) Shift if being multiplied by a power of 2
+*/
